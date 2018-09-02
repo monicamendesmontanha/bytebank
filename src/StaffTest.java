1570d392775540cef8b280bec1063acfb5fb2c47
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-03 09:31:52 +1000

    rename name and surname

--- src/StaffTest.java
+++ src/StaffTest.java
@@ -2,13 +2,13 @@ public class StaffTest {
 
     public static void main(String[] args) {
 
-        Staff nico = new Staff();
+        Staff john = new Staff();
 
-        nico.setName("John Doe");
-        nico.setId(("2240335644-9"));
-        nico.setSalary(2600.00);
+        john.setName("John Doe");
+        john.setId(("2240335644-9"));
+        john.setSalary(2600.00);
 
-        System.out.println(nico.getName());
-        System.out.println(nico.getBonus());
+        System.out.println(john.getName());
+        System.out.println(john.getBonus());
     }
 }
