051385a83d1d03a6666f59983fd06b8ae5f44827
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-03 12:49:58 +1000

    add manager test

---
+++ src/Manager.java
@@ -0,0 +1,21 @@
+public class Manager extends Staff {
+
+    private int password;
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public boolean authenticates (int password) {
+        if (this.password == password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public double getBonus() {
+        return super.getBonus() + super.getSalary();
+    }
+}
---
+++ src/ManagerTest.java
@@ -0,0 +1,22 @@
+public class ManagerTest {
+
+    public static void main(String[] args) {
+
+        Manager m1 = new Manager();
+        m1.setName("Dan Doe");
+        m1.setId("23252710-0");
+        m1.setSalary(5000.0);
+
+        System.out.println("name: " + m1.getName());
+        System.out.println("id: " + m1.getId());
+        System.out.println("salary: " + m1.getSalary());
+
+        m1.setPassword(2222);
+        boolean authenticated = m1.authenticates (2222);
+
+        System.out.println(authenticated);
+
+        System.out.println("bonus: " + m1.getBonus());
+
+    }
+}
