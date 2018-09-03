49b12c19d71b79f66c3ba2aec1081c43f6f96104
Mônica Mendes <monicamendesmontanha@gmail.com>
2018-09-03 18:46:15 +1000

    introduce customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,26 @@
 public class Customer {
+
+    private String name;
+    private String id;
+    private String occupation;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getOccupation() {
+        return occupation;
+    }
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
 }
